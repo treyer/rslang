24f66c6325bcaa12fd00c519e246a1746c7cfaac
MarinaPresmytskaia <mpresmytskaia@gmail.com>
2022-02-07 01:13:47 +0300

    feat: add hover logic to level button

--- rslang/src/pages/textbook/textbook.tsx
+++ rslang/src/pages/textbook/textbook.tsx
@@ -1,20 +1,33 @@
 import { Button, Typography } from '@mui/material';
-import React from 'react';
+import React, { MouseEvent, useState } from 'react';
 
 import './textbook.scss';
 
 import { ENGLISH_LEVELS } from './consts';
 
+type TLevelId = '0' | '1' | '2' | '3' | '4' | '5';
+
 const Textbook = () => {
+  const [activeLevel, satActiveLevel] = useState<TLevelId>('0');
+  const handleLevelHover = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    const activeLevelId = target.id as TLevelId;
+    satActiveLevel(activeLevelId);
+    console.error(e);
+  };
+
   return (
     <div className="App-textbook">
       <Typography variant="h3">Уровни сложности</Typography>
       <div className="textbook_levels-list">
         <section className="textbook_level-list-section-button">
-          {ENGLISH_LEVELS.map((level) => (
+          {Object.values(ENGLISH_LEVELS).map((level) => (
             <Button
+              id={level.id}
+              key={level.id}
               variant="contained"
-              className={`textbook_btn textbook_button-${level.levelId}`}
+              className={`textbook_btn textbook_button-${level.id}`}
+              onMouseEnter={handleLevelHover}
             >
               <span className="textbook_code-title">{level.code}</span>
               <span className="textbook_level-title">{level.level}</span>
@@ -22,19 +35,13 @@ const Textbook = () => {
           ))}
         </section>
         <section className="textbook_level-list-section-inform">
-          <img src="" alt="" />
-          <p className="textbook_level-list-text">Элементрарный уровень</p>
+          <img
+            src="/assets/png/elementary.png"
+            alt="Elementary"
+            className="textbook_img"
+          />
           <p className="textbook_level-list-text">
-            На этом уровне вы можете понимать употребительные выражения на общие
-            темы, такие как: семья, покупки, работа и др. Разговаривать на
-            простые повседневные темы, выражаясь простыми фразами. Рассказать
-            простыми выражениями о себе, описывать простые ситуации.
-            Телепередачи на английском не будут понятны, разве что отдельные
-            слова, но собеседника, если он будет говорить отчетливо, простыми
-            фразами в 2-3 слова в общем поймете.Уровень Elementary можно назвать
-            «уровнем для выживания», Survival English. Его достаточно для того,
-            чтобы «выжить» во время поездки в страну, где основной язык
-            английский.
+            {ENGLISH_LEVELS[activeLevel].text}
           </p>
         </section>
       </div>
