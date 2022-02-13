ab72324006b200cb91d4d624b570d32a922cf180
MarinaPresmytskaia <mpresmytskaia@gmail.com>
2022-02-13 20:31:41 +0300

    refactor: move EnglishLevelButton to common

--- rslang/src/pages/games/GameSprint/GameSprint.tsx
+++ rslang/src/pages/games/GameSprint/GameSprint.tsx
@@ -1,7 +1,7 @@
 import { MouseEvent, useCallback, useState } from 'react';
 import { Typography } from '@mui/material';
-import EnglishLevelButton from '../../textbook/components/EnglishLevelButton/EnglishLevelButton';
-import { ENGLISH_LEVELS } from '../../textbook/consts';
+import EnglishLevelButton from '../../../Components/EnglishLevelButton/EnglishLevelButton';
+import { ENGLISH_LEVELS } from '../../../General/constants';
 import './GameSprint.scss';
 
 type TGameId = keyof typeof ENGLISH_LEVELS;
